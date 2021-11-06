46a6f7c985fc0984a307c81c993ad85851472db5
Hikmahx <hikmayousuph@gmail.com>
2021-11-06 14:11:59 +0100

    function for displaying menu

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import Home from './pages/Home'
 
 
 function App() {
+  const displayMenu = ()=>{
+    let hamburger = document.querySelector('.hamburger-menu');
+    let menu= document.querySelector('.menu');
+    hamburger.classList.toggle('active')
+    menu.classList.toggle('active')
+  }
+  
   return(
     <div className='App relative w-100  overflow-hidden'>
-      <Navbar/>
+      <Navbar displayMenu={displayMenu}/>
       <Home/>
     </div>
 
