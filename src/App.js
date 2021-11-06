5695c02ef29a4ba7e30306b12589e5c0f378d593
Hikmahx <hikmayousuph@gmail.com>
2021-11-06 13:01:40 +0100

    home and nav component imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+// import {BrowserRouter as Router, Switch, Route} from 'react-dom'
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
+
 
 function App() {
-  return <div className='App'>
-  </div>;
+  return(
+    <div className='App relative w-100  overflow-hidden'>
+      <Navbar/>
+      <Home/>
+    </div>
+
+    // <div className='App'>
+    //   <Switch>
+    //   <Route exact path='/' render={()=>
+    //     <Home/>
+    //     }/>        
+        
+    //   <Route exact path='/details' render={()=>
+    //     <h1>details</h1>
+    //   }/>
+    //   </Switch>
+    // </div>
+    // </Router>
+    )
 }
 
 export default App;
