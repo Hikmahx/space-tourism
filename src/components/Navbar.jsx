d6a844f1fe7c85012b9274af30dc5c3a19220588
Hikmahx <hikmayousuph@gmail.com>
2021-11-09 20:58:42 +0100

    a turned to Link, activeLine function prop added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import logo from '../assets/shared/logo.svg'
 import hamburger from '../assets/shared/icon-hamburger.svg'
 import close from '../assets/shared/icon-close.svg'
+import { Link } from 'react-router-dom'
 
-const Navbar = ({displayMenu}) => {
+const Navbar = ({displayMenu, activeLine}) => {
   return (
     <header className='absolute lg:top-10 z-10 sm:pl-12 sm:ml-2 flex items-center justify-between w-full font-barlow'>
       <div className='wrapper relative flex items-center justify-between w-full'>
@@ -15,19 +16,23 @@ const Navbar = ({displayMenu}) => {
           <img className="hamburger-icon w-full" src={hamburger} alt="icon" />
           <img className="close-icon w-full" src={close} alt="icon" />
         </div>
-        <nav className="menu flex-1 absolute sm:relative top-0 pt-28 sm:pt-0 pl-8 sm:pl-0 right-0 left-24 lg:left-0 ">
+        <nav onClick={activeLine} className="menu flex-1 absolute sm:relative top-0 pt-28 sm:pt-0 pl-8 sm:pl-0 right-0 left-24 lg:left-0 ">
           <ul className='flex sm:items-center sm:justify-center text-white pr-8 uppercase h-screen sm:h-24 flex-col sm:flex-row text-lg lg:text-base font-thin'>
             <li className="mx-2 lg:mx-3 xl:mx-5 lg:h-full flex items-center my-3 active">
-              <a href='/' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">00</strong> Home</a>
+            <Link to={'/'} className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">00</strong> Home</Link>
+              {/* <a href='/' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">00</strong> Home</a> */}
             </li>
             <li className="mx-2 lg:mx-3 xl:mx-5 lg:h-full flex items-center my-3">
-              <a href='/destination' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">01</strong> Destination</a>
+              <Link to={'/destination'} className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">01</strong> Destination</Link>
+              {/* <a href='/destination' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">01</strong> Destination</a> */}
             </li>
             <li className="mx-2 lg:mx-3 xl:mx-5 lg:h-full flex items-center my-3">
-              <a href='/crew' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">02</strong> Crew</a>
+            <Link to={'/crew'} className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">02</strong> Crew</Link>
+              {/* <a href='/crew' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">02</strong> Crew</a> */}
             </li>
             <li className="mx-2 lg:mx-3 xl:mx-5 lg:h-full flex items-center my-3">
-              <a href='/technology' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">03</strong> Technology</a>
+            <Link to={'/technology'} className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">03</strong> Technology</Link>
+              {/* <a href='/technology' className="whitespace-nowrap"><strong className="mr-2 sm:hidden xl:inline font-semibold">03</strong> Technology</a> */}
             </li>
           </ul>
         </nav>
