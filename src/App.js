2df3d46f37bcf9bde0bf3ba2208b922a7caaface
Hikmahx <hikmayousuph@gmail.com>
2021-11-14 18:58:25 +0100

    getDestination function removed

--- src/App.js
+++ src/App.js
@@ -26,45 +26,6 @@ function App() {
     })
   }
 
-  const getDestination = () =>{
-    let tabLinks = document.querySelectorAll('.tab-link')
-
-    let tabImg = document.querySelectorAll('.img-wrapper img');
-    let tabHeader = document.querySelectorAll('.tab-body h2');
-    let tabDesc = document.querySelectorAll('.tab-body p');
-    let tabInfo = document.querySelectorAll('.tab-body .tab-info');
-    
-    tabImg[0].classList.add('active');
-    tabHeader[0].classList.add('active');
-    tabDesc[0].classList.add('active');
-    tabInfo[0].classList.add('active');
-
-    for (let number = 0; number < tabLinks.length; number++) {
-      tabLinks[number].addEventListener('click', ()=>{
-        tabLinks.forEach(link => {
-          link.classList.remove('active');
-        });
-        tabImg.forEach(img => {
-          img.classList.remove('active');
-        });
-        tabHeader.forEach(header=>{
-          header.classList.remove('active');
-        });
-        tabDesc.forEach(desc=>{
-          desc.classList.remove('active');
-        });
-        tabInfo.forEach(info=>{
-          info.classList.remove('active');
-        });
-        
-        tabLinks[number].classList.add('active');
-        tabImg[number].classList.add('active');    
-        tabHeader[number].classList.add('active');    
-        tabDesc[number].classList.add('active');    
-        tabInfo[number].classList.add('active');    
-      })
-    }
-  }
   
   return(
     <Router>
@@ -73,9 +34,7 @@ function App() {
 
         <Routes>
           <Route exact path='/' element={<Home/>}/>   
-          <Route exact path='/destination' element={
-          <Destination getDestination={getDestination} />
-          }/>   
+          <Route exact path='/destination' element={<Destination/>}/>   
           <Route exact path='/crew' element={<Crew/>}/>   
           <Route exact path='/technology' element={<Technology/>}/>   
         </Routes>
