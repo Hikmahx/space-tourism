3f52706e0eca1d17f522aa4fd1e6bbb17b780698
Hikmahx <hikmayousuph@gmail.com>
2021-11-15 20:34:23 +0100

    tablet-view completed

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -13,31 +13,31 @@ const Crew = () => {
         <img className="absolute hidden md:block lg:hidden top-0 bottom-0 w-full h-full" src={bgTablet} alt="" />
         <img className="absolute hidden md:hidden lg:block top-0 bottom-0 w-full h-full" src={bgDesktop} alt="" />
 
-        <div className="relative slider mt-20 pb-28 pt-1 font-barlow text-white xl:px-20">
-          <h1 className="uppercase mb-10 text-base sm:text-2xl text-center sm:text-left"><strong className="mr-4 text-grayish-blue xl:font-bold">02</strong>meet your crew</h1>
+        <div className="relative slider mt-20 pb-28 md:pb-0 pt-1 font-barlow text-white xl:px-20">
+          <h1 className="uppercase mb-10 text-base sm:text-2xl text-center sm:text-left sm:px-10 sm:pt-8"><strong className="mr-4 text-grayish-blue xl:font-bold">02</strong>meet your crew</h1>
           <div className="container flex flex-col xl:flex-row text-center xl:text-left items-center justify-center mx-auto">
 
             <div className="slider-right md:order-2 w-4/5 xl:w-1/2">
-              <div className="slider-img-wrapper px-16 max-w-24">
-                <img className="px-2" src={`${crew[0].images.png.slice(1)}`} alt="slider-img"/>         
+              <div className="slider-img-wrapper px-16 max-w-xl mx-auto">
+                <img className="px-2 sm:px-5" src={`${crew[0].images.png.slice(1)}`} alt="slider-img"/>         
               </div>
               <div className="line bg-white mx-auto md:hidden"></div>
             </div>
             <div className="slider-left md:order-1 w-4/5 xl:w-1/2 flex flex-col">
               <div className="slider-info order-2 md:order-1">
-                <h2 className="uppercase font-belleFair text-2xl sm:text-7xl lg:text-8xl sm:mt-6 py-1 xl:mb-6">
-                  <span style={{color:'#61636a'}} className="block text-base mb-1">
+                <h2 className="uppercase font-belleFair text-2xl sm:text-4xl sm:mt-6 py-1 xl:mb-6">
+                  <span style={{color:'#61636a'}} className="block text-base sm:text-2xl mb-1 sm:mb-3">
                     {crew[0].role}
                   </span>
                   <span className="block mb-3">
                     {crew[0].name}
                   </span>
                 </h2>
-                <p className="text-light-blue text-base">
+                <p className="text-light-blue text-base sm:text-lg max-w-lg mx-auto">
                   {crew[0].bio}
                 </p>
               </div>
-              <div className="manual-navigation order-1 md:order-2 w-full h-auto flex items-center justify-center lg:justify-start mt-6 mb-4">
+              <div className="manual-navigation order-1 md:order-2 w-full h-auto flex items-center justify-center lg:justify-start mt-6 sm:mt-7 mb-4">
                 <span className="btn btn-1 w-3 h-3 rounded-full m-2 active"></span>
                 <span className="btn btn-2 w-3 h-3 rounded-full m-2"></span>
                 <span className="btn btn-3 w-3 h-3 rounded-full m-2"></span>
