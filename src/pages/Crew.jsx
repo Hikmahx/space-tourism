2cd85290c4172968abdf80a5af1a9b8f20fe9d2d
Hikmahx <hikmayousuph@gmail.com>
2021-11-17 17:11:17 +0100

    desktop-view completed

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -8,40 +8,40 @@ const Crew = () => {
 
   return (
     <div>
-      <div className="crew-wrapper xl:min-h-screen lg:overflow-hidden">
+      <div className="crew-wrapper lg:overflow-hidden">
         <img className="absolute md:hidden top-0 bottom-0 w-full h-full" src={bgMobile} alt="" />
         <img className="absolute hidden md:block lg:hidden top-0 bottom-0 w-full h-full" src={bgTablet} alt="" />
         <img className="absolute hidden md:hidden lg:block top-0 bottom-0 w-full h-full" src={bgDesktop} alt="" />
 
-        <div className="relative slider mt-20 pb-28 md:pb-0 pt-1 font-barlow text-white xl:px-20">
-          <h1 className="uppercase mb-10 text-base sm:text-2xl text-center sm:text-left sm:px-10 sm:pt-8"><strong className="mr-4 text-grayish-blue xl:font-bold">02</strong>meet your crew</h1>
-          <div className="container flex flex-col xl:flex-row text-center xl:text-left items-center justify-center mx-auto">
+        <div className="relative slider mt-20 pb-28 md:pb-0 pt-1 font-barlow text-white lg:px-10 xl:px-20 xl:mx-20 lg:flex lg:flex-col lg:justify-end">
+          <h1 className="uppercase mb-10 xl:mb-0 text-base sm:text-2xl xl:text-3xl text-center sm:text-left sm:px-10 lg:px-0 sm:pt-8 xl:mt-2 xl:absolute xl:top-24"><strong className="mr-4 text-grayish-blue xl:font-bold">02</strong>meet your crew</h1>
+          <div className="container flex flex-col lg:flex-row text-center lg:text-left items-center justify-center mx-auto">
 
             <div className="slider-right md:order-2 w-4/5 xl:w-1/2">
-              <div className="slider-img-wrapper px-16 max-w-xl mx-auto">
-                <img className="px-2 sm:px-5" src={`${crew[0].images.png.slice(1)}`} alt="slider-img"/>         
+              <div className="slider-img-wrapper px-16 xl:px-4 max-w-xl lg:max-w-2xl mx-auto">
+                <img className="px-2 sm:px-5 lg:px-0 xl:pl-6 xl:mx-auto" src={`${crew[0].images.png.slice(1)}`} alt="slider-img"/>         
               </div>
               <div className="line bg-white mx-auto md:hidden"></div>
             </div>
-            <div className="slider-left md:order-1 w-4/5 xl:w-1/2 flex flex-col">
+            <div className="slider-left md:order-1 w-4/5 xl:w-1/2 flex flex-col xl:mt-20">
               <div className="slider-info order-2 md:order-1">
-                <h2 className="uppercase font-belleFair text-2xl sm:text-4xl sm:mt-6 py-1 xl:mb-6">
-                  <span style={{color:'#61636a'}} className="block text-base sm:text-2xl mb-1 sm:mb-3">
+                <h2 className="uppercase font-belleFair text-2xl sm:text-4xl xl:text-6xl sm:mt-6 py-1 xl:mb-6">
+                  <span style={{color:'#61636a'}} className="block text-base sm:text-3xl xl:text-3xl mb-1 sm:mb-3 xl:mb-5">
                     {crew[0].role}
                   </span>
                   <span className="block mb-3">
                     {crew[0].name}
                   </span>
                 </h2>
-                <p className="text-light-blue text-base sm:text-lg max-w-lg mx-auto">
+                <p className="text-light-blue text-base sm:text-lg xl:text-xl max-w-lg xl:max-w-md mx-auto lg:mx-0 xl:tracking-wider">
                   {crew[0].bio}
                 </p>
               </div>
-              <div className="manual-navigation order-1 md:order-2 w-full h-auto flex items-center justify-center lg:justify-start mt-6 sm:mt-7 mb-4">
-                <span className="btn btn-1 w-3 h-3 rounded-full m-2 active"></span>
-                <span className="btn btn-2 w-3 h-3 rounded-full m-2"></span>
-                <span className="btn btn-3 w-3 h-3 rounded-full m-2"></span>
-                <span className="btn btn-4 w-3 h-3 rounded-full m-2"></span>
+              <div className="manual-navigation order-1 md:order-2 w-full h-auto flex items-center justify-center lg:justify-start mt-6 sm:mt-7 mb-4 xl:mt-28">
+                <span className="btn btn-1 w-3 h-3 xl:w-4 xl:h-4 rounded-full m-2 xl:m-3 active"></span>
+                <span className="btn btn-2 w-3 h-3 xl:w-4 xl:h-4 rounded-full m-2 xl:m-3"></span>
+                <span className="btn btn-3 w-3 h-3 xl:w-4 xl:h-4 rounded-full m-2 xl:m-3"></span>
+                <span className="btn btn-4 w-3 h-3 xl:w-4 xl:h-4 rounded-full m-2 xl:m-3"></span>
               </div>
             </div>
           </div>
